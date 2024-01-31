24f570242df560e009bed7e64993386fd8d7b7f1
quangshuynh <20378quang@gmail.com>
2024-01-31 02:27:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    
+  $("#blue-btn").click(function(){
+    $("h2").toggleClass("blue-font");
+  });
+
+  $("#green-btn").click(function(){
+    $("h2").toggleClass("green-font");
+  });
+
+});
